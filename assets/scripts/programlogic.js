a320f4046b0a4eb86e365f19e4bdf91194aecc35
“wilsonkaya” <fatihkaya2@yahoo.com>
2017-02-18 14:47:51 -0500

    Add program logic but is not in use

--- assets/scripts/programlogic.js
+++ assets/scripts/programlogic.js
@@ -1,7 +1,15 @@
 "use strict";
 
 
-
+// let newHTML = "";
+// const print = function (data){
+//   let fatih = data
+//   fatih.forEach(function(account){
+//     newHTML += "<li><h4>" + account.web_name + "</h4> By " + account.password_hint + "</em>'</li>";
+//   });
+//   $("#show-info").html(newHTML);
+// };
+//
 
 
 
@@ -36,5 +44,6 @@
 
 
 module.exports = {
+  print,
 
 }
