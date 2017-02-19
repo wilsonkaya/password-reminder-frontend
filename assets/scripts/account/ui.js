cedfe912820850fd500b3aef803d9f9fffe1a755
“wilsonkaya” <fatihkaya2@yahoo.com>
2017-02-19 14:27:02 -0500

    Define timers for onPatchSuccess

--- assets/scripts/account/ui.js
+++ assets/scripts/account/ui.js
@@ -15,18 +15,22 @@ const onDeleteSuccess = function () {
 };
 
 
-const onPatchSuccess = (data) => {
-  console.log(data);
-  // console.log(data);
-  // let showBooksHtml = showBooksTemplate({ accounts: data.accounts });
-  // $('.content').append(showBooksHtml);
+const onPatchSuccess = () => {
+  $(".warning-update-data").text("Succesfull !");
+  setTimeout(function() {$('.kapat').modal('hide');}, 1300);
+  setTimeout(function() {$('.warning-update-data').text("");}, 1300);
 
 };
+
 
 const onPostSuccess = function (data) {
   $("#warning-create-new-data").text("Succesfull !");
+  setTimeout(function() {$('#myModal-create-password').modal('hide');}, 1300);
+  setTimeout(function() {$('.clean-create').val("");}, 1300);
+  setTimeout(function() {$('#warning-create-new-data').text("");}, 1300);
   console.log(data);
 };
+
 
 const onPostError = function() {
   $("#warning-create-new-data").text("Please fill in !");
