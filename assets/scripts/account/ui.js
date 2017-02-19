99bb4f7dadf5875f4868fa5b5ff9a39d8b6389c4
“wilsonkaya” <fatihkaya2@yahoo.com>
2017-02-18 21:54:45 -0500

    Re-organize ui

--- assets/scripts/account/ui.js
+++ assets/scripts/account/ui.js
@@ -3,13 +3,9 @@ const showBooksTemplate = require('../templates/account-listing.handlebars');
 // const logic = require("../programlogic.js");
 const onSuccess = (data) => {
 
-  console.log(data);
+
   let showBooksHtml = showBooksTemplate({ accounts: data.accounts });
   $('.content').append(showBooksHtml);
-
-// console.log(data)
-//   logic.print(data);
-
 
 };
 
@@ -18,11 +14,16 @@ const onError = function (response) {
 };
 
 const onDeleteSuccess = function () {
-  console.log('Book was successfully deleted.');
+  console.log('Account was successfully deleted.');
 };
 
-const onPatchSuccess = function () {
-  console.log('Book was successfully patched.');
+
+const onPatchSuccess = (data) => {
+  console.log(data);
+  // console.log(data);
+  // let showBooksHtml = showBooksTemplate({ accounts: data.accounts });
+  // $('.content').append(showBooksHtml);
+
 };
 
 const onPostSuccess = function (data) {
